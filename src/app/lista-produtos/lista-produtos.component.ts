9afe53e3567a850a3cb25ff4994f62bb84f331a3
Ricardo <ricardosr.mota@gmail.com>
2018-06-17 23:04:41 -0300

    Atualizacao tela Produto

--- src/app/lista-produtos/lista-produtos.component.ts
+++ src/app/lista-produtos/lista-produtos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DisciplinasService } from '../admin/disciplinas.service';
 
 @Component({
   selector: 'app-lista-produtos',
@@ -6,10 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-produtos.component.css']
 })
 export class ListaProdutosComponent implements OnInit {
+  disciplinas: any[];
+  carrinho =[];
 
-  constructor() { }
+  id = null;
+  codigo = null;
+  nome = null;
+  preco = null;
+  url = null;
+  categoria=null;
+  editando=null;
+  vendo=false;
+ 
+
+  constructor(private disciplinasService: DisciplinasService) { }
 
   ngOnInit() {
+    this.atualizarLista();
+    
   }
+  atualizarLista() {
+    this.disciplinasService.getDisciplinas()
+      .subscribe(disciplinas => this.disciplinas = disciplinas);
+  }
+
+  ver(disciplina) {
+    this.vendo=true;
+    this.nome = disciplina.nome;
+    this.preco = disciplina.preco;
+    this.url = disciplina.url;
+    this.categoria=disciplina.categoria;
+}
+cancelar(){
+  this.vendo=false;
+}
+comprar(disciplina)
+{
+  this.carrinho.push(disciplina);
+}
 
 }
